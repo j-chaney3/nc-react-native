bc831c98bc578ec7080062572e6526eaa36dc8da
Jame Chaney <macneilslt@gmail.com>
2023-06-06 17:15:13 -0400

    Using Redux

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import Main from './screens/MainComponent';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 //console.disableYellowBox = true; //uncomment to disable yellow box dev warnings if wanted
 
 export default function App() {
 	return (
-		<NavigationContainer>
-			<Main />
-		</NavigationContainer>
+		<Provider store={store}>
+			<NavigationContainer>
+				<Main />
+			</NavigationContainer>
+		</Provider>
 	);
 }
