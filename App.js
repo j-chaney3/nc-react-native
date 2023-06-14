08d5039f03929b1b87912a6e368ec8f0b348ef98
Jame Chaney <macneilslt@gmail.com>
2023-06-14 16:12:11 -0400

    persist redux store

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import Main from './screens/MainComponent';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import Loading from './components/LoadingComponent';
 //console.disableYellowBox = true; //uncomment to disable yellow box dev warnings if wanted
-
+//
 export default function App() {
 	return (
 		<Provider store={store}>
-			<NavigationContainer>
-				<Main />
-			</NavigationContainer>
+			<PersistGate lading={<Loading />} persistor={persistor}>
+				<NavigationContainer>
+					<Main />
+				</NavigationContainer>
+			</PersistGate>
 		</Provider>
 	);
 }
